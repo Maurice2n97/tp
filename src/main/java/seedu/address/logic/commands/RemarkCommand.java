54b5a7e6441d1b565cf752ac214c97783f8f955b
Mok Bing Wei Maurice <Maurice2n97@gmail.com>
2021-02-27 09:14:37 +0800

    Add Remark Command
    Follow Tutorial 3 guidelines, add in remark command, test it and ensure unit tests are working, implement some features.

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -1,10 +1,17 @@
 package seedu.address.logic.commands;
 
+import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
+import seedu.address.model.person.Person;
+import seedu.address.model.person.Remark;
+
+import java.util.List;
 
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_REMARK;
+import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 /**
      * Changes the remark of an existing person in the address book.
@@ -12,17 +19,25 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
     public class RemarkCommand extends Command {
 
         public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
-
         public static final String COMMAND_WORD = "remark";
 
+        public static final String MESSAGE_USAGE = COMMAND_WORD + ":" + " adds a remark under a person at a certain index. "
+                + " Parameters : \n"
+                + "1 "
+                + PREFIX_REMARK
+                + "It's the anime man";
+
         private final Index index;
-        private final String remark;
+        private final Remark remark;
+
+        public static final String MESSAGE_ADD_REMARK_SUCCESS = "MARKED SUCCESS";
+        public static final String MESSAGE_DELETE_REMARK_SUCCESS = "MESSAGE DELETED!";
 
         /**
          * @param index of the person in the filtered person list to edit the remark
          * @param remark of the person to be updated to
          */
-        public RemarkCommand(Index index, String remark) {
+        public RemarkCommand(Index index, Remark remark) {
             requireAllNonNull(index, remark);
 
             this.index = index;
@@ -30,7 +45,25 @@ import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
         }
         @Override
         public CommandResult execute(Model model) throws CommandException {
-            throw new CommandException(String.format(MESSAGE_ARGUMENTS, index.getOneBased(), remark));
+            List<Person> lastShownList = model.getFilteredPersonList();
+
+            if (index.getZeroBased() >= lastShownList.size()) {
+                throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            }
+
+            Person personToEdit = lastShownList.get(index.getZeroBased());
+            Person editedPerson = new Person(personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
+                    personToEdit.getAddress(),personToEdit.getTags(), remark);
+
+            model.setPerson(personToEdit, editedPerson);
+            model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+            return new CommandResult(generateSuccessMessage(editedPerson));
+        }
+
+        private String generateSuccessMessage(Person personToEdit) {
+            String message = !remark.value.isEmpty() ? MESSAGE_ADD_REMARK_SUCCESS : MESSAGE_DELETE_REMARK_SUCCESS;
+            return String.format(message, personToEdit);
         }
 
         @Override
--- src/main/java/seedu/address/logic/parser/ParserUtil.java
+++ src/main/java/seedu/address/logic/parser/ParserUtil.java
@@ -9,10 +9,7 @@ import java.util.Set;
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.StringUtil;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Phone;
+import seedu.address.model.person.*;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -120,5 +117,15 @@ public class ParserUtil {
             tagSet.add(parseTag(tagName));
         }
         return tagSet;
+    }
+
+    public static Remark parseRemark(String args) throws ParseException{
+        requireNonNull(args);
+        String trimmedString = args.trim();
+        if (!Remark.isValidRemark(trimmedString)) {
+            throw new ParseException(Remark.MESSAGE_CONSTRAINTS);
+        } else {
+            return new Remark(trimmedString);
+        }
     }
 }
